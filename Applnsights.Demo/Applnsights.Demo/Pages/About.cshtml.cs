568f375f9b68a1318e41765c3577feb7b72f9590
Gaurav Madaan <gaurav.madaan@technossus.com>
2018-11-21 11:27:14 +0530

    Basic configuration of App Insights

---
+++ Applnsights.Demo/Applnsights.Demo/Pages/About.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Applnsights.Demo.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+            throw new Exception("Collecting insights by codestellar");
+        }
+    }
+}
---
+++ powershell/invoke-website.ps1
@@ -0,0 +1,4 @@
+for ($i = 0 ; $i -lt 100; $i++)
+{
+ Invoke-WebRequest -uri http://localhost:52583/
+}
